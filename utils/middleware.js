33f8fc63fec05cbf4690e3f91657f299bdfde80b
arnaud kaderi <arnay07@gmail.com>
2022-12-08 18:40:11 +0100

    added user management

--- utils/middleware.js
+++ utils/middleware.js
@@ -19,7 +19,7 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message });
-  }
+  }else if ()
   next(error);
 };
 
